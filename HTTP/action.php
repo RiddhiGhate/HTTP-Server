a2bbdc94d19563a6341e24a88e7582edfd462994
RiddhiGhate <56785559+RiddhiGhate@users.noreply.github.com>
2021-07-26 14:05:59 +0530

    Delete HTTP directory

--- HTTP/action.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $fname = htmlspecialchars($_POST['fname']);
-  $lname  = htmlspecialchars($_POST['lname']);
-
-  echo  $say, ' ', $to;
-?>
--- HTTP/post_data.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import os
-fileitem = form['fileToUpload']
-if fileitem.fileToUpload:
-	fn = os.path.basename(fileitem.fileToUpload)
-	open(fn, 'wb').write(fileitem.file.read())
